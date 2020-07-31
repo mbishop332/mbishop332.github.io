28ee0e8689a187e80a26c4c7d58f5581760048ff
mbishop332 <69055387+mbishop332@users.noreply.github.com>
2020-07-31 15:26:47 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -9,21 +9,54 @@ var heroes = [
 	'Rey from Star Wars',
 	'Ariana Grande',
 	'Sonic the Hedgehog',
-	'Mario and Luigi'
+	'Mario and Luigi',
+	'Simba from the Lion King',
+	'Cinderella',
+	'Bella Swan from Twilight',
+
 
 ]
 
 var villains = [
-'Rasputin',
+'Rasputin, the mystic who advised the last czar of Russia',
 'Billie Eilish',
 'the Paw Patrol dogs',
 'Dora the Explorer',
+'Draco Malfoy',
+'Kylo Ren from Star Wars',
+'Edward Cullen from Twilight',
+'Benjamin Franklin, 18th century inventor and Founding Father of the United States',
+'Alexander Hamilton, 18th century economist and Founding Father of the United States',
+'Iron Man',
+
+
+
 ]
 
 var places = [
 'a busy coffeeshop',
 'a small New England town',
-'a secret passageway'
+'a secret passageway',
+'Toledo, Ohio',
+'a school for witchcraft and wizardry',
+'a car on a road trip',
+'a city bus',
+'the middle of a desert',
+'a pirate ship',
+'the International Space Station',
+'the Moon',
+'a galaxy far, far away',
+'18th century Virginia',
+'the middle of the woods',
+'outside during a snowstorm',
+'a haunted house',
+'an Escape Room at the mall',
+'a high school football game',
+'a grocery store',
+'an animal shelter',
+'on an Alaskan dogsled race',
+'a cruise ship',
+'a mysterious cottage',
 ]
 
 function newHero() {
